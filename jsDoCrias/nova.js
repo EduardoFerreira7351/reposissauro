9cf745f4e4136cf6e7bb0ed087b557bbba786df8
Eduardo Ferreira <eduardo.ferreirar@sempreceub.com>
2024-08-22 12:27:42 -0300

    shadow o ouriço

---
+++ jsDoCrias/nova.js
@@ -0,0 +1,16 @@
+var pãoComLinguiça = "buchinchei";    //teste 1
+
+// teste 2
+var primeiroNome = "Eduardo" ;
+var ultimo_nome = "Ramos!";
+var soma = "primeiroNome + ultimo_nome";
+alert ("meu nome completo é :" + soma);
+
+//teste 3
+let x = 5;
+let y = 5;
+let z = 6;
+("x==y")
+("x==z")
+
+//teste 4

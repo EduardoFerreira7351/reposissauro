9b1508d7bf50f9af6a460f1f6f326ed4eb228c90
Eduardo Ferreira <eduardo.ferreirar@sempreceub.com>
2024-08-27 01:19:11 -0300

    att portfolio

--- jsDoCrias/nova.js
+++ jsDoCrias/nova.js
@@ -1,16 +1,6 @@
-var pãoComLinguiça = "buchinchei";    //teste 1
-
-// teste 2
-var primeiroNome = "Eduardo" ;
-var ultimo_nome = "Ramos!";
-var soma = "primeiroNome + ultimo_nome";
-alert ("meu nome completo é :" + soma);
-
-//teste 3
-let x = 5;
-let y = 5;
-let z = 6;
-("x==y")
-("x==z")
-
-//teste 4
+function paraCelsius (firenheit){
+    let conversao = (5/9) * (firenheit = 32);
+    return conversao;
+}
+let valor = paraCelsius (77);
+console.log ("a temperature é : " + valor);
